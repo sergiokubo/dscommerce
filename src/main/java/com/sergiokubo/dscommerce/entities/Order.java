7e1fa96a3de7b506b2b58e2d64593e32171cd7e2
sergiokubo <sergio.t.kubo@gmail.com>
2025-02-06 23:18:38 -0300

    Entity Payment created

--- src/main/java/com/sergiokubo/dscommerce/entities/Order.java
+++ src/main/java/com/sergiokubo/dscommerce/entities/Order.java
@@ -20,10 +20,13 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
@@ -60,5 +63,13 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
