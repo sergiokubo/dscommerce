f09bb7d4880943418bfddd01945fb787398672fd
sergiokubo <sergio.t.kubo@gmail.com>
2025-02-16 16:31:34 -0300

    POST new product

--- src/main/java/com/sergiokubo/dscommerce/controllers/ProductController.java
+++ src/main/java/com/sergiokubo/dscommerce/controllers/ProductController.java
@@ -5,12 +5,7 @@ import com.sergiokubo.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -28,6 +23,11 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto){
+        return service.insert(dto);
     }
 
 }
